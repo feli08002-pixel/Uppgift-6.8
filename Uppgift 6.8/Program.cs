c9110cd65c369fe3b42ffc1f51a47002e576a23b
feli08002 <feli08002@E-PW05SJ7G>
2026-01-20 22:20:06 +0100

    Add project files.

---
+++ Uppgift 6.8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_6._8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(AntalTalIText("5 4,1 hej 9,04"));
+        }
+
+        static int AntalTalIText(string text)
+        {
+            string[] delar = text.Split(' ');
+            int antal = 0;
+
+            foreach (string d in delar)
+                if (double.TryParse(d, out _))
+                    antal++;
+
+            return antal;
+        }
+    }
+}
